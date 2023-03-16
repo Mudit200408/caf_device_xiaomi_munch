56605c02c632deab49c690fc486af60ee0626796
Mudit200408 <muditpurohit2004.pm@gmail.com>
2023-03-16 13:40:48 +0530

    Revert "munch: configs: Drop fpc and goodix fingerprint keylayouts"
    This reverts commit ff81e001e74196a4e54c16700394bc9762c66b21.

--- device.mk
+++ device.mk
@@ -250,7 +250,7 @@ PRODUCT_COPY_FILES += \
 
 # Keylayout
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/keylayout/,$(TARGET_COPY_OUT_VENDOR)/usr/keylayout)
 
 # Keymaster
 PRODUCT_PACKAGES += \
